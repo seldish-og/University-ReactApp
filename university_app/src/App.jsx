a06413f800571bcc476a18c01f4b6f9cb981391a
george <aurelian@georges-MacBook-Air.local>
2024-02-02 13:52:31 +0300

    all done

--- university_app/src/App.jsx
+++ university_app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 // Импортируем компоненты Welcome и Button
 import { Button } from './components/Button/Button';
+import { ColorChanger } from './components/ColoredText/ColoredText';
 import { Message } from './components/Message/Message';
 import { Welcome } from './components/Welcome/Welcome';
 
@@ -13,6 +14,7 @@ function App() {
         <Welcome name="Egor" />
         <Button />
         <Message/>
+        <ColorChanger/>
       </div>
     );
 }
--- university_app/src/components/ColoredText/ColoredText.jsx
+++ university_app/src/components/ColoredText/ColoredText.jsx
@@ -1,6 +1,30 @@
+import React, { useState } from 'react';
 
-// Создаем функциональный компонент Welcome
-export const Welcome = (props) => {
+function ColorChangeFunc() {
+  const [color, setColor] = useState('');
+
+  function handleChange(event) {
+    setColor(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    document.getElementById('text').style.color = color;
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="color-input">Enter a color:</label>
+        <input type="text" id="color-input" value={color} onChange={handleChange} />
+        <button type="submit">Change Text Color</button>
+      </form>
+      <p id="text">This is some text.</p>
+    </div>
+  );
+}
+
+export const ColorChanger = () => {
     // Возвращаем JSX-элемент с использованием props
-    return <h1>ChangeColor</h1>;
+    return ColorChangeFunc();
   }
